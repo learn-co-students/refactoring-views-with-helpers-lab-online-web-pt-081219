57d933464deb9eb3745de4906caf6121f3ba3e50
wifelette <LeahSilber@gmail.com>
2020-01-30 10:22:44 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,9 @@
 module ArtistsHelper
+  def display_artist(song)
+    if song.artist.nil? || song.artist.name.blank?
+      link_to "Add Artist", edit_song_path(song)
+    else
+      link_to "By #{song.artist.name}", artist_path(song.artist)
+    end
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,18 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    # Only keep going if the thing on the left (self.artist) is not nil. If it's not nil, keep on moving and do the `.name`.
+    # This is called a Safe Navigation Operator. 
+    self.artist&.name
   end
 
   def artist_name=(name)
+    if self.artist
+      self.artist.name = name
+    elsif (artist = Artist.find_by(name: name))
+      self.artist = artist
+    else
+      self.create_artist(name: name)
+    end
   end
 end
